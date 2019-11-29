b938cb266d607cc706e95807c43284051a7d122b
Thanh Nguyen <nguyenhm.thanh@gmail.com>
2019-11-29 15:15:17 -0800

    add build

---
+++ build/precache-manifest.9ea552613fd727651d755b60efba78f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4364ab1000d645ccc22f702d7196b864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c53fc2f95a768f6b045",
+    "url": "/static/css/2.9d6dfeae.chunk.css"
+  },
+  {
+    "revision": "b225f0741dbfd363d843",
+    "url": "/static/css/main.75827616.chunk.css"
+  },
+  {
+    "revision": "1c53fc2f95a768f6b045",
+    "url": "/static/js/2.1c1a11d9.chunk.js"
+  },
+  {
+    "revision": "b225f0741dbfd363d843",
+    "url": "/static/js/main.3e54ff80.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "ddfab15459f4eb0b1dfb4319529839b4",
+    "url": "/static/media/no_image.ddfab154.png"
+  }
+]);
