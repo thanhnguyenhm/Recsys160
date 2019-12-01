d2549bae68f50d18e8c1cea505e8f2137235cd49
Thanh Nguyen <nguyenhm.thanh@gmail.com>
2019-11-30 21:32:02 -0800

    hard code backend api url for all modules

--- src/config.js
+++ src/config.js
@@ -3,9 +3,11 @@
 
 const API_URL = 'https://api.themoviedb.org/3/movie/';
 const API_KEY = '250de1e3739e381ada22002544ae28fd';
+const BACKEND_API = "https://recsys160.herokuapp.com/"
 
 
 export {
   API_URL,
   API_KEY,
+  BACKEND_API,
 }
